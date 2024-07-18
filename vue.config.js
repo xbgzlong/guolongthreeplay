d86d6f35b540707621960e5e55024d067ef3c20f
阿龙 <935850064@qq.com>
2024-07-18 08:37:39 +0800

    texiao

--- vue.config.js
+++ vue.config.js
@@ -20,7 +20,7 @@ module.exports = {
 	// 如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。
 	// 从 Vue CLI 3.3 起已弃用，请使用publicPath。
 	// baseUrl :process.env.NODE_ENV === "production" ? "/" : "/",
-	publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+	publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
 	// 在npm run build 或 yarn build 时 ，生成文件的目录名称（要和baseUrl的生产环境路径一致）（默认dist）
 	outputDir: 'dist',
 	// 用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
