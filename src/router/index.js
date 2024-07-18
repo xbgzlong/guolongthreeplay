1eb2e8d829254ec7a2b6f469c4f48e2b564429a4
阿龙 <935850064@qq.com>
2024-07-18 08:58:17 +0800

    texiao

--- src/router/index.js
+++ src/router/index.js
@@ -60,7 +60,7 @@ Router.prototype.replace = function push(location) {
 }
 
 export default new Router({
-	mode: 'history', // 去掉url中的#
+	// mode: 'history', // 去掉url中的#
 	scrollBehavior: () => ({ y: 0 }),
 	routes: routes
 })
